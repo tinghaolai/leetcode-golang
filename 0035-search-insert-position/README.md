3b758aef3aa31665fd96b573d02a77e27fd88386
Ting-Hao Lai <analth355@gmail.com>
2021-10-15 22:43:15 +0800

    0035 Adding online solution

--- 0035-search-insert-position/README.md
+++ 0035-search-insert-position/README.md
@@ -6,3 +6,7 @@ Memory Usage: 3 MB, less than 43.53% of Go online submissions for Search Insert 
 ### Self solution
 
 > 過於直覺，沒什麽好寫的
+
+### Online solution 1
+
+> 基本上就是二元樹的解法，時間複雜度比較低
--- 0035-search-insert-position/search-insert-position.go
+++ 0035-search-insert-position/search-insert-position.go
@@ -10,3 +10,18 @@ func searchInsert(nums []int, target int) int {
     return len(nums)
 }
 
+func searchInsert_1(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if nums[mid] >= target {
+			high = mid - 1
+		} else {
+			if (mid == len(nums)-1) || (nums[mid+1] >= target) {
+				return mid + 1
+			}
+			low = mid + 1
+		}
+	}
+	return 0
+}
