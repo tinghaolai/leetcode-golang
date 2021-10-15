7927c253f0900c3a9e802ea28ff21f978421d19d
Ting-Hao Lai <analth355@gmail.com>
2021-10-15 22:23:10 +0800

    Fix solution shouldn't use switch case due to has only one case

--- 0035-search-insert-position/search-insert-position.go
+++ 0035-search-insert-position/search-insert-position.go
@@ -2,8 +2,7 @@ package leetcode
 
 func searchInsert(nums []int, target int) int {
     for i, v := range nums {
-        switch {
-            case target == v || target < v:
+        if (target == v || target < v) {
                 return i
         }
     }
