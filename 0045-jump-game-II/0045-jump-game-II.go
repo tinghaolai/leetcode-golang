4b98aecce84223f4ed8612be058c1d2fd3ddb9b5
Ting-Hao Lai <analth355@gmail.com>
2021-10-17 01:35:49 +0800

    0045 add online solution and comment

--- 0045-jump-game-II/0045-jump-game-II.go
+++ 0045-jump-game-II/0045-jump-game-II.go
@@ -24,3 +24,23 @@ func jump(nums []int) int {
 
     return step
 }
+
+func jump_1(nums []int) int {
+	if len(nums) == 1 {
+		return 0
+	}
+	needChoose, canReach, step := 0, 0, 0
+	for i, x := range nums {
+		if i+x > canReach {
+			canReach = i + x
+			if canReach >= len(nums)-1 {
+				return step + 1
+			}
+		}
+		if i == needChoose {
+			needChoose = canReach
+			step++
+		}
+	}
+	return step
+}
