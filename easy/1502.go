f1ed6319397462ea4685a31a41ea06d7cc62146a
tinghaolai <analth355@gmail.com>
2023-06-06 09:22:25 +0800

    Add solution fot 1502

---
+++ easy/1502.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func canMakeArithmeticProgression(arr []int) bool {
+	sort.Ints(arr)
+	diff := arr[1] - arr[0]
+	for i := 1; i < len(arr)-1; i++ {
+		if arr[i+1]-arr[i] != diff {
+			return false
+		}
+	}
+
+	return true
+}
